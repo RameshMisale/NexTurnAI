176de3503e6d44602df0672166ed66ce3d986c8c
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-25 17:56:20 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -13,10 +13,20 @@ st.markdown(
     f"""
     <style>
         .stApp {{
-            background-color: black; /* Black background color */
+            background-color: #f2f2f2; /* App background color */
         }}
         .images-box {{
-            background-color: #F0FFFF; /* Azure background color */
+            background-color: #EED9C7; /* Almond background color */
+            padding: 10px;
+            border-radius: 10px;
+        }}
+        .videos-box {{
+            background-color: #963939; /* Antique Ruby background color */
+            padding: 10px;
+            border-radius: 10px;
+        }}
+        .audios-box {{
+            background-color: #8DB051; /* Artichoke Green background color */
             padding: 10px;
             border-radius: 10px;
         }}
@@ -54,7 +64,7 @@ with col1:
 
 # Case: Videos
 with col2:
-    st.info("Videos")
+    st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
     st.markdown("Video 1")
     st.markdown("[Link to Video 1](https://www.example.com/video1)")
     st.markdown("Video 2")
@@ -64,7 +74,7 @@ with col2:
 
 # Case: Audios
 with col3:
-    st.info("Audios")
+    st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
     st.markdown("Audio 1")
     st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
     st.markdown("Audio 2")
