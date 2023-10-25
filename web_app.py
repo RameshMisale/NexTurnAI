651059ec9d142006b7636a82797840477bbdac62
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-25 18:07:11 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -4,7 +4,7 @@ from PIL import Image
 # Set page title and company logo
 st.set_page_config(
     page_title="NexTurn AI Project",
-    page_icon=":rocket:",
+    page_icon=":rocket:", 
     layout="wide"
 )
 
