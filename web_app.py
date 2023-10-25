0accf83410ea1a396c477b4c0bf67f16928a01ae
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-25 17:30:23 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -27,25 +27,57 @@ col1, col2, col3 = st.columns(3)
 
 # Case One: Images
 with col1:
-    st.header("Case One (Images)")
-    st.image("image_example.jpg", width=300)
-    st.markdown("[Link to Case One](https://innodataengineers.wordpress.com)")
+    st.header("Image 1")
+    st.image("image_example_1.jpg", width=300)
+    st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
 
-# Case Two: Videos
+# Case Two: Images
 with col2:
-    st.header("Case Two (Videos)")
-    st.video("video_example.mp4")
-    st.markdown("[Link to Case Two](https://www.example.com/case2)")
+    st.header("Image 2")
+    st.image("image_example_2.jpg", width=300)
+    st.markdown("[Link to Image 2](https://www.example.com/image2)")
 
-# Case Three: Audios
+# Case Three: Images
 with col3:
-    st.header("Case Three (Audios)")
-    st.audio("audio_example.mp3")
-    st.markdown("[Link to Case Three](https://www.example.com/case3)")
+    st.header("Image 3")
+    st.image("image_example_3.jpg", width=300)
+    st.markdown("[Link to Image 3](https://www.example.com/image3)")
 
-# Add a fun fact
-st.sidebar.header("Fun Fact")
-st.sidebar.write("Did you know? Streamlit is an amazing tool for creating interactive web applications with Python!")
+# Case Four: Videos
+with col1:
+    st.header("Video 1")
+    st.video("video_example_1.mp4")
+    st.markdown("[Link to Video 1](https://www.example.com/video1)")
+
+# Case Five: Videos
+with col2:
+    st.header("Video 2")
+    st.video("video_example_2.mp4")
+    st.markdown("[Link to Video 2](https://www.example.com/video2)")
+
+# Case Six: Videos
+with col3:
+    st.header("Video 3")
+    st.video("video_example_3.mp4")
+    st.markdown("[Link to Video 3](https://www.example.com/video3)")
+
+# Case Seven: Audios
+with col1:
+    st.header("Audio 1")
+    st.audio("audio_example_1.mp3")
+    st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
+
+# Case Eight: Audios
+with col2:
+    st.header("Audio 2")
+    st.audio("audio_example_2.mp3")
+    st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
+
+# Case Nine: Audios
+with col3:
+    st.header("Audio 3")
+    st.audio("audio_example_3.mp3")
+    st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
 
 # Footer
 st.markdown("---")
