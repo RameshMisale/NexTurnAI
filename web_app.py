c04e72bb874b1d7f5506e1a53fe3233ad6fc0691
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-25 18:14:13 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -50,38 +50,41 @@ st.image(image, width=desired_width, use_column_width=True)
 st.title("Welcome to NexTurn AI Project!")
 st.write("Explore different cases and navigate to specific links.")
 
-# Define the layout for the cases
-col1, col2, col3 = st.columns(3)
+# Create a horizontal layout container
+container = st.container()
 
 # Case: Images
-with col1:
-    st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
-    st.markdown("Image 1")
-    st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
-    st.markdown("Image 2")
-    st.markdown("[Link to Image 2](https://www.example.com/image2)")
-    st.markdown("Image 3")
-    st.markdown("[Link to Image 3](https://www.example.com/image3)")
+with container:
+    with st.columns(3):
+        st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
+        st.markdown("Image 1")
+        st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
+        st.markdown("Image 2")
+        st.markdown("[Link to Image 2](https://www.example.com/image2)")
+        st.markdown("Image 3")
+        st.markdown("[Link to Image 3](https://www.example.com/image3)")
 
 # Case: Videos
-with col2:
-    st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
-    st.markdown("Video 1")
-    st.markdown("[Link to Video 1](https://www.example.com/video1)")
-    st.markdown("Video 2")
-    st.markdown("[Link to Video 2](https://www.example.com/video2)")
-    st.markdown("Video 3")
-    st.markdown("[Link to Video 3](https://www.example.com/video3)")
+with container:
+    with st.columns(3):
+        st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
+        st.markdown("Video 1")
+        st.markdown("[Link to Video 1](https://www.example.com/video1)")
+        st.markdown("Video 2")
+        st.markdown("[Link to Video 2](https://www.example.com/video2)")
+        st.markdown("Video 3")
+        st.markdown("[Link to Video 3](https://www.example.com/video3)")
 
 # Case: Audios
-with col3:
-    st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
-    st.markdown("Audio 1")
-    st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
-    st.markdown("Audio 2")
-    st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
-    st.markdown("Audio 3")
-    st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
+with container:
+    with st.columns(3):
+        st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
+        st.markdown("Audio 1")
+        st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
+        st.markdown("Audio 2")
+        st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
+        st.markdown("Audio 3")
+        st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
 
 # Footer
 st.markdown("---")
