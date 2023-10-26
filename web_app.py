9070144025269de243a2b3875eea5bb83d200996
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-26 12:55:51 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,104 +1,164 @@
+[12:50] Ramesh Misale
 import streamlit as st
+
 from PIL import Image
-
+ 
 # Set page title and company logo
+
 st.set_page_config(
+
     page_title="NexTurn AI Project",
+
     page_icon=":rocket:",
+
     layout="wide"
-)
 
-# Define the background color and custom font using CSS
+)
+ 
+# Define the background color using CSS
+
 st.markdown(
+
     f"""
+
     <style>
+
         .stApp {{
-            background-color: #2A2A2A; /* Dark background color */
-            color: #FFFFFF; /* Text color */
-            font-family: 'Arial', sans-serif; /* Custom font */
+
+            background-color: black; /* Set the background color to black */
+
+            color: white; /* Text color */
+
         }}
+
         .container {{
+
             display: flex;
+
             flex-direction: row;
+
         }}
-        .box {{
-            background-color: #34495E; /* Blue-Grey background color */
-            padding: 20px;
+
+        .images-box {{
+
+            background-color: #EED9C7; /* Almond background color */
+
+            padding: 10px;
+
             border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3); /* Shadow effect */
-            margin-right: 20px;
+
+            margin-right: 10px;
+
         }}
-        .box a {{
-            text-decoration: none;
-            color: #ECF0F1; /* Link color */
-            font-weight: bold;
+
+        .videos-box {{
+
+            background-color: #963939; /* Antique Ruby background color */
+
+            padding: 10px;
+
+            border-radius: 10px;
+
+            margin-right: 10px;
+
         }}
-        .box a:hover {{
-            color: #E74C3C; /* Hover effect color */
+
+        .audios-box {{
+
+            background-color: #8DB051; /* Artichoke Green background color */
+
+            padding: 10px;
+
+            border-radius: 10px;
+
         }}
-        .box h3 {{
-            color: #ECF0F1; /* Section title color */
-            margin-bottom: 10px;
-            font-size: 24px;
-        }}
+
     </style>
+
     """,
+
     unsafe_allow_html=True,
+
 )
-
+ 
 # Open the image file
+
 image = Image.open("Logo.png")
-
+ 
 # Get the dimensions of the image
+
 width, height = image.size
-
+ 
 # Calculate the desired width (in this example, 500 pixels)
-desired_width = 500
 
+desired_width = 50
+ 
 # Add a title and description
-st.image(image, width=desired_width, use_column_width=True)
-st.title("Welcome to NexTurn AI Project!")
-st.write("Explore different cases and navigate to specific links.")
 
+st.image(image, width=desired_width, use_column_width=True)
+
+st.title("Welcome to NexTurn AI Project!")
+
+st.write("Explore different cases and navigate to specific links.")
+ 
 # Create a horizontal layout container using custom CSS
+
 st.markdown('<div class="container">')
 
 # Case: Images
-st.markdown('<div class="box">')
-st.markdown('<h3>Images</h3>', unsafe_allow_html=True)
+
+st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
+
 st.markdown("Image 1")
-st.markdown("[Link to Image 1](https://www.example.com/image1)")
+
+st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
+
 st.markdown("Image 2")
+
 st.markdown("[Link to Image 2](https://www.example.com/image2)")
+
 st.markdown("Image 3")
+
 st.markdown("[Link to Image 3](https://www.example.com/image3)")
-st.markdown('</div>')
-
+ 
 # Case: Videos
-st.markdown('<div class="box">')
-st.markdown('<h3>Videos</h3>', unsafe_allow_html=True)
+
+st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
+
 st.markdown("Video 1")
+
 st.markdown("[Link to Video 1](https://www.example.com/video1)")
+
 st.markdown("Video 2")
+
 st.markdown("[Link to Video 2](https://www.example.com/video2)")
+
 st.markdown("Video 3")
+
 st.markdown("[Link to Video 3](https://www.example.com/video3)")
-st.markdown('</div>')
-
+ 
 # Case: Audios
-st.markdown('<div class="box">')
-st.markdown('<h3>Audios</h3>', unsafe_allow_html=True)
+
+st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
+
 st.markdown("Audio 1")
+
 st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
+
 st.markdown("Audio 2")
+
 st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
+
 st.markdown("Audio 3")
+
 st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
-st.markdown('</div>')
-
+ 
 # Close the horizontal layout container
-st.markdown('</div>')
 
+st.markdown('</div>')
+ 
 # Footer
+
 st.markdown("---")
+
 st.write("Thank you for visiting...!!")
