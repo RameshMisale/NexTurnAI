7faf4255348e7a06317e06699977d618819c059f
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-26 13:11:40 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from PIL import Image
+
 # Set page title and company logo
-st.set_page_config(page_title="NexTurn AI Project",page_icon=":rocket:",layout="wide")
- 
+st.set_page_config(
+    page_title="NexTurn AI Project",
+    page_icon=":rocket:",
+    layout="wide"
+)
+
 # Define the background color using CSS
 st.markdown(
     f"""
     <style>
         .stApp {{
-            background-color: black; /* Set the background color to black */
+            background-color: transparent; /* Set the background color to transparent */
             color: white; /* Text color */
+            background-image: url('background.jpg'); /* Background image */
+            background-size: cover;
+            background-attachment: fixed;
         }}
         .container {{
             display: flex;
@@ -35,19 +43,25 @@ st.markdown(
     </style>
     """,
     unsafe_allow_html=True,
-) 
+)
+
 # Open the image file
-image = Image.open("Logo.png") 
+image = Image.open("Logo.png")
+
 # Get the dimensions of the image
-width, height = image.size 
+width, height = image.size
+
 # Calculate the desired width (in this example, 500 pixels)
-desired_width = 50 
+desired_width = 50
+
 # Add a title and description
 st.image(image, width=desired_width, use_column_width=True)
 st.title("Welcome to NexTurn AI Project!")
-st.write("Explore different cases and navigate to specific links.") 
+st.write("Explore different cases and navigate to specific links.")
+
 # Create a horizontal layout container using custom CSS
-#st.markdown('<div class="container">')
+st.markdown('<div class="container">')
+
 # Case: Images
 st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
 st.markdown("Image 1")
@@ -56,6 +70,7 @@ st.markdown("Image 2")
 st.markdown("[Link to Image 2](https://www.example.com/image2)")
 st.markdown("Image 3")
 st.markdown("[Link to Image 3](https://www.example.com/image3)")
+
 # Case: Videos
 st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
 st.markdown("Video 1")
@@ -64,6 +79,7 @@ st.markdown("Video 2")
 st.markdown("[Link to Video 2](https://www.example.com/video2)")
 st.markdown("Video 3")
 st.markdown("[Link to Video 3](https://www.example.com/video3)")
+
 # Case: Audios
 st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
 st.markdown("Audio 1")
@@ -71,9 +87,11 @@ st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
 st.markdown("Audio 2")
 st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
 st.markdown("Audio 3")
-st.markdown("[Link to Audio 3](https://www.example.com/audio3)") 
+st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
+
 # Close the horizontal layout container
-#st.markdown('</div>') 
+st.markdown('</div>')
+
 # Footer
 st.markdown("---")
 st.write("Thank you for visiting...!!")
