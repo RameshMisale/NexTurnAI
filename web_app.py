1bc8bc101a198de783f53acb14fa076b34d0767f
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-30 14:49:18 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 
 # Logo
-st.image("Logo.png") 
+st.image("Logo.png")
 st.title("Welcome to NexTurn AI Project")
 
 # Create tabs for cases
@@ -17,35 +17,44 @@ tabs = st.tabs(["Case 1", "Case 2", "Case 3"])
 if tabs == "Case 1":
     st.write("Content for Case 1")
 
-    # Container for Case 1
-    with st.container():
+    # Create columns for content
+    col1, col2 = st.columns(2)
+
+    with col1:
         st.subheader("Details for Case 1")
         st.write("This is the content for Case 1.")
         st.markdown("You can add more content here.")
 
-    # Open a new tab with HTML link
-    st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
+    with col2:
+        # Open a new tab with HTML link
+        st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
 
 elif tabs == "Case 2":
     st.write("Content for Case 2")
 
-    # Container for Case 2
-    with st.container():
+    # Create columns for content
+    col1, col2 = st.columns(2)
+
+    with col1:
         st.subheader("Details for Case 2")
         st.write("This is the content for Case 2.")
         st.markdown("You can add more content here.")
 
-    # Open a new tab with HTML link
-    st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
+    with col2:
+        # Open a new tab with HTML link
+        st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
 
 elif tabs == "Case 3":
     st.write("Content for Case 3")
 
-    # Container for Case 3
-    with st.container():
+    # Create columns for content
+    col1, col2 = st.columns(2)
+
+    with col1:
         st.subheader("Details for Case 3")
         st.write("This is the content for Case 3.")
         st.markdown("You can add more content here.")
 
-    # Open a new tab with HTML link
-    st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
+    with col2:
+        # Open a new tab with HTML link
+        st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
