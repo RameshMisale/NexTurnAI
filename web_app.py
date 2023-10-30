fbb50db412a47af3e2f5603aa3b8f93230534ad5
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-30 14:50:17 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -7,8 +7,8 @@ st.set_page_config(
     layout="wide"
 )
 
-# Logo
-st.image("Logo.png")
+# Logo and title
+st.image("Logo.png", width=100)
 st.title("Welcome to NexTurn AI Project")
 
 # Create tabs for cases
@@ -17,44 +17,76 @@ tabs = st.tabs(["Case 1", "Case 2", "Case 3"])
 if tabs == "Case 1":
     st.write("Content for Case 1")
 
-    # Create columns for content
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.subheader("Details for Case 1")
-        st.write("This is the content for Case 1.")
-        st.markdown("You can add more content here.")
-
-    with col2:
-        # Open a new tab with HTML link
-        st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="custom-container">
+            <div class="case-content">
+                <h3>Details for Case 1</h3>
+                <p>This is the content for Case 1.</p>
+            </div>
+            <div class="case-link">
+                <a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 elif tabs == "Case 2":
     st.write("Content for Case 2")
 
-    # Create columns for content
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.subheader("Details for Case 2")
-        st.write("This is the content for Case 2.")
-        st.markdown("You can add more content here.")
-
-    with col2:
-        # Open a new tab with HTML link
-        st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="custom-container">
+            <div class="case-content">
+                <h3>Details for Case 2</h3>
+                <p>This is the content for Case 2.</p>
+            </div>
+            <div class="case-link">
+                <a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 elif tabs == "Case 3":
     st.write("Content for Case 3")
 
-    # Create columns for content
-    col1, col2 = st.columns(2)
+    st.markdown(
+        """
+        <div class="custom-container">
+            <div class="case-content">
+                <h3>Details for Case 3</h3>
+                <p>This is the content for Case 3.</p>
+            </div>
+            <div class="case-link">
+                <a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
-    with col1:
-        st.subheader("Details for Case 3")
-        st.write("This is the content for Case 3.")
-        st.markdown("You can add more content here.")
-
-    with col2:
-        # Open a new tab with HTML link
-        st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
+# Custom CSS to style the layout
+st.markdown(
+    """
+    <style>
+        .custom-container {
+            display: flex;
+            justify-content: space-between;
+            margin: 20px;
+            padding: 10px;
+            border: 1px solid #ddd;
+        }
+        .case-content {
+            flex: 2;
+        }
+        .case-link {
+            flex: 1;
+            text-align: right;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
