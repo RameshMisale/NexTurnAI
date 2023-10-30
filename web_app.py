653b70b2e7e016c8bd46679f977c7632e309749a
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-30 14:43:08 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import Image
 
 # Set page title and company logo
 st.set_page_config(
@@ -8,49 +7,22 @@ st.set_page_config(
     layout="wide"
 )
 
-# Open the image file
-image = Image.open("Logo.png")
+# Logo
+st.image("path_to_your_logo.png", width=100)
+st.title("Welcome to NexTurn AI Project")
 
-# Get the dimensions of the image
-width, height = image.size
+# Create tabs for cases
+tabs = st.tabs(["Case 1", "Case 2", "Case 3"])
 
-# Calculate the desired width (in this example, 500 pixels)
-desired_width = 50
-
-# Add a title and description
-st.image(image, width=desired_width, use_column_width=True)
-st.title("Welcome to NexTurn AI Project!")
-st.write("Explore different cases and navigate to specific links.")
-
-# Create a horizontal layout container using custom CSS
-st.markdown('<div class="container">')
-
-# Create tabs for Images and Videos
-tabs = st.tabs(["Images", "Videos"])
-
-if tabs == "Images":
-    st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
-    st.title("Image Section")
-    st.markdown("Image 1")
-    st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
-    st.markdown("Image 2")
-    st.markdown("[Link to Image 2](https://www.example.com/image2)")
-    st.markdown("Image 3")
-    st.markdown("[Link to Image 3](https://www.example.com/image3)")
-
-elif tabs == "Videos":
-    st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
-    st.title("Video Section")
-    st.markdown("Video 1")
-    st.markdown("[Link to Video 1](https://www.example.com/video1)")
-    st.markdown("Video 2")
-    st.markdown("[Link to Video 2](https://www.example.com/video2)")
-    st.markdown("Video 3")
-    st.markdown("[Link to Video 3](https://www.example.com/video3)")
-
-# Close the horizontal layout container
-st.markdown('</div>')
-
-# Footer
-st.markdown("---")
-st.write("Thank you for visiting...!!")
+if tabs == "Case 1":
+    st.write("Content for Case 1")
+    # Open a new tab with HTML link
+    st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
+elif tabs == "Case 2":
+    st.write("Content for Case 2")
+    # Open a new tab with HTML link
+    st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
+elif tabs == "Case 3":
+    st.write("Content for Case 3")
+    # Open a new tab with HTML link
+    st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
