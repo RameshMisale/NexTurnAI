682bbc72f491545434f9eb5ab4fdc865ec66171f
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-30 14:22:29 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -64,40 +64,41 @@ st.title("Welcome to NexTurn AI Project!")
 st.write("Explore different cases and navigate to specific links.")
 
 # Create a horizontal layout container using custom CSS
-#st.markdown('<div class="container">')
+st.markdown('<div class="container">')
 
-# Case: Images
-st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
-st.title("Image Section")
-st.markdown("Image 1")
-st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
-st.markdown("Image 2")
-st.markdown("[Link to Image 2](https://www.example.com/image2)")
-st.markdown("Image 3")
-st.markdown("[Link to Image 3](https://www.example.com/image3)")
+# Create tabs for Images, Videos, and Audios
+tabs = st.tabs(["Images", "Videos", "Audios"])
 
-# Case: Videos
-st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
-st.title("Video Section")
-st.markdown("Video 1")
-st.markdown("[Link to Video 1](https://www.example.com/video1)")
-st.markdown("Video 2")
-st.markdown("[Link to Video 2](https://www.example.com/video2)")
-st.markdown("Video 3")
-st.markdown("[Link to Video 3](https://www.example.com/video3)")
-
-# Case: Audios
-st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
-st.title("Audio Section")
-st.markdown("Audio 1")
-st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
-st.markdown("Audio 2")
-st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
-st.markdown("Audio 3")
-st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
+if tabs == "Images":
+    st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
+    st.title("Image Section")
+    st.markdown("Image 1")
+    st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
+    st.markdown("Image 2")
+    st.markdown("[Link to Image 2](https://www.example.com/image2)")
+    st.markdown("Image 3")
+    st.markdown("[Link to Image 3](https://www.example.com/image3)")
+elif tabs == "Videos":
+    st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
+    st.title("Video Section")
+    st.markdown("Video 1")
+    st.markdown("[Link to Video 1](https://www.example.com/video1)")
+    st.markdown("Video 2")
+    st.markdown("[Link to Video 2](https://www.example.com/video2)")
+    st.markdown("Video 3")
+    st.markdown("[Link to Video 3](https://www.example.com/video3)")
+else:
+    st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
+    st.title("Audio Section")
+    st.markdown("Audio 1")
+    st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
+    st.markdown("Audio 2")
+    st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
+    st.markdown("Audio 3")
+    st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
 
 # Close the horizontal layout container
-#st.markdown('</div>')
+st.markdown('</div>')
 
 # Footer
 st.markdown("---")
