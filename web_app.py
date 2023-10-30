71bc36f6aeb1c9ee1c1ded55f48c26d39028bebc
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-30 15:05:42 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,24 +1,24 @@
 import streamlit as st
 
-# Create tabs using st.radio
-selected_tab = st.radio("Select a tab:", ["Tab 1", "Tab 2", "Tab 3"])
+# Create tabs
+tab1, tab2, tab3 = st.columns(3)
 
-# Content for Tab 1
-if selected_tab == "Tab 1":
-    st.header("Tab 1 Content")
-    st.write("This is the content for Tab 1.")
-    st.image("image1.jpg", use_column_width=True)
+with tab1:
+    if st.button("Tab 1"):
+        st.header("Tab 1 Content")
+        st.write("This is the content for Tab 1.")
+        st.image("image1.jpg", use_column_width=True)
 
-# Content for Tab 2
-elif selected_tab == "Tab 2":
-    st.header("Tab 2 Content")
-    st.write("This is the content for Tab 2.")
-    st.write("You can add more elements here.")
-    st.image("image2.jpg", use_column_width=True)
+with tab2:
+    if st.button("Tab 2"):
+        st.header("Tab 2 Content")
+        st.write("This is the content for Tab 2.")
+        st.write("You can add more elements here.")
+        st.image("image2.jpg", use_column_width=True)
 
-# Content for Tab 3
-elif selected_tab == "Tab 3":
-    st.header("Tab 3 Content")
-    st.write("This is the content for Tab 3.")
-    st.write("You can add more elements here.")
-    st.image("image3.jpg", use_column_width=True)
+with tab3:
+    if st.button("Tab 3"):
+        st.header("Tab 3 Content")
+        st.write("This is the content for Tab 3.")
+        st.write("You can add more elements here.")
+        st.image("image3.jpg", use_column_width=True)
