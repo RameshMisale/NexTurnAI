70325f611146f68d38ee00408ed3b621faa409a9
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-30 14:26:46 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import Image
 
 # Set page title and company logo
 st.set_page_config(
@@ -50,16 +49,16 @@ st.markdown(
 )
 
 # Open the image file
-image = Image.open("Logo.png")
+# image = Image.open("Logo.png")
 
 # Get the dimensions of the image
-width, height = image.size
+# width, height = image.size
 
 # Calculate the desired width (in this example, 500 pixels)
 desired_width = 50
 
 # Add a title and description
-st.image(image, width=desired_width, use_column_width=True)
+# st.image(image, width=desired_width, use_column_width=True)
 st.title("Welcome to NexTurn AI Project!")
 st.write("Explore different cases and navigate to specific links.")
 
@@ -72,30 +71,15 @@ tabs = st.tabs(["Images", "Videos", "Audios"])
 if tabs == "Images":
     st.markdown('<div class="images-box">Images</div>', unsafe_allow_html=True)
     st.title("Image Section")
-    st.markdown("Image 1")
-    st.markdown("[Link to Image 1](https://innodataengineers.wordpress.com)")
-    st.markdown("Image 2")
-    st.markdown("[Link to Image 2](https://www.example.com/image2)")
-    st.markdown("Image 3")
-    st.markdown("[Link to Image 3](https://www.example.com/image3)")
+    # Placeholder for images or content
 elif tabs == "Videos":
     st.markdown('<div class="videos-box">Videos</div>', unsafe_allow_html=True)
     st.title("Video Section")
-    st.markdown("Video 1")
-    st.markdown("[Link to Video 1](https://www.example.com/video1)")
-    st.markdown("Video 2")
-    st.markdown("[Link to Video 2](https://www.example.com/video2)")
-    st.markdown("Video 3")
-    st.markdown("[Link to Video 3](https://www.example.com/video3)")
+    # Placeholder for videos or content
 else:
     st.markdown('<div class="audios-box">Audios</div>', unsafe_allow_html=True)
     st.title("Audio Section")
-    st.markdown("Audio 1")
-    st.markdown("[Link to Audio 1](https://www.example.com/audio1)")
-    st.markdown("Audio 2")
-    st.markdown("[Link to Audio 2](https://www.example.com/audio2)")
-    st.markdown("Audio 3")
-    st.markdown("[Link to Audio 3](https://www.example.com/audio3)")
+    # Placeholder for audios or content
 
 # Close the horizontal layout container
 st.markdown('</div>')
