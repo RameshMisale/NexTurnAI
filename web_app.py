a21f7960d62318eb0a29bebde739532f7bc2304b
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-31 14:04:15 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -16,7 +16,7 @@ tabs1, tabs2, tabs3 = st.tabs(["Case 1", "Case 2", "Case 3"])
 #st.write(tabs)
 
 if tabs1:
-    st.write("Content for Case 1")
+    st.write("Case 1")
 
     # Display cat image under Case 1 (use the local file path)
     st.image("cat.jpg", use_column_width=True)
@@ -26,22 +26,22 @@ if tabs1:
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.subheader("Details for Case 1")
+        st.subheader("Case 1")
         st.write("This is the content for Case 1.")
         st.markdown("You can add more content here.")
 
     with col2:
-        st.subheader("Column 2")
+        st.subheader("Case 2")
         st.write("You can add content here.")
         st.markdown("Additional information goes here.")
 
     with col3:
-        st.subheader("Column 3")
+        st.subheader("Case 3")
         st.write("You can add content here.")
         st.markdown("More information goes here.")
 
 if tabs2:
-    st.write("Content for Case 2")
+    st.write("Case 2")
 
     # Display dog image under Case 2 (use the local file path)
     st.image("dog.jpg", use_column_width=True)
@@ -51,17 +51,17 @@ if tabs2:
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.subheader("Details for Case 2")
+        st.subheader("Case 2")
         st.write("This is the content for Case 2.")
         st.markdown("You can add more content here.")
 
     with col2:
-        st.subheader("Column 2")
+        st.subheader("Case 2")
         st.write("You can add content here.")
         st.markdown("Additional information goes here.")
 
     with col3:
-        st.subheader("Column 3")
+        st.subheader("Case 3")
         st.write("You can add content here.")
         st.markdown("More information goes here.")
 
@@ -76,16 +76,16 @@ if tabs3:
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.subheader("Details for Case 3")
+        st.subheader("Case 3")
         st.write("This is the content for Case 3.")
         st.markdown("You can add more content here.")
 
     with col2:
-        st.subheader("Column 2")
+        st.subheader("Case 2")
         st.write("You can add content here.")
         st.markdown("Additional information goes here.")
 
     with col3:
-        st.subheader("Column 3")
+        st.subheader("Case 3")
         st.write("You can add content here.")
         st.markdown("More information goes here.")
