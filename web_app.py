c4329de1389e320298efbeab33fe85d5d1b447ae
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-31 16:48:36 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -14,64 +14,62 @@ st.title("Welcome to NexTurn AI Project")
 # Create tabs for sections
 tabs = st.tabs(["Images Cases", "Videos Cases", "Audios Cases"])
 
-# Create containers for each section
-with st.container():
-    if tabs == "Images Cases":
-        st.write("Images Cases")
-        # Create columns for content
-        col1, col2, col3 = st.columns(3)
+if tabs == "Images Cases":
+    st.write("Images Cases")
 
-        with col1:
+    # Create containers for each case
+    with st.container():
+        with st.container():
             st.subheader("Case 1")
             st.write("This is the content for Images Case 1.")
             st.markdown("You can add more content here.")
 
-        with col2:
+        with st.container():
             st.subheader("Case 2")
             st.write("You can add content here for Images Case 2.")
             st.markdown("Additional information goes here.")
 
-        with col3:
+        with st.container():
             st.subheader("Case 3")
             st.write("You can add content here for Images Case 3.")
             st.markdown("More information goes here.")
 
-    elif tabs == "Videos Cases":
-        st.write("Videos Cases")
-        # Create columns for content
-        col1, col2, col3 = st.columns(3)
+elif tabs == "Videos Cases":
+    st.write("Videos Cases")
 
-        with col1:
+    # Create containers for each case
+    with st.container():
+        with st.container():
             st.subheader("Case 1")
             st.write("This is the content for Videos Case 1.")
             st.markdown("You can add more content here.")
 
-        with col2:
+        with st.container():
             st.subheader("Case 2")
             st.write("You can add content here for Videos Case 2.")
             st.markdown("Additional information goes here.")
 
-        with col3:
+        with st.container():
             st.subheader("Case 3")
             st.write("You can add content here for Videos Case 3.")
             st.markdown("More information goes here.")
 
-    elif tabs == "Audios Cases":
-        st.write("Audios Cases")
-        # Create columns for content
-        col1, col2, col3 = st.columns(3)
+elif tabs == "Audios Cases":
+    st.write("Audios Cases")
 
-        with col1:
+    # Create containers for each case
+    with st.container():
+        with st.container():
             st.subheader("Case 1")
             st.write("This is the content for Audios Case 1.")
             st.markdown("You can add more content here.")
 
-        with col2:
+        with st.container():
             st.subheader("Case 2")
             st.write("You can add content here for Audios Case 2.")
             st.markdown("Additional information goes here.")
 
-        with col3:
+        with st.container():
             st.subheader("Case 3")
             st.write("You can add content here for Audios Case 3.")
             st.markdown("More information goes here.")
