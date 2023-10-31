eabdd8355e1c19772b35229306c93699a44496ef
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-10-31 17:17:51 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -35,44 +35,44 @@ if tabs1:
         st.write("You can add content here.")
         st.markdown("https://www.example.com", unsafe_allow_html=True)
 
-if tabs2:
-    st.write("Sample Cases")
+# if tabs2:
+#     st.write("Sample Cases")
 
-    # Create columns for content
-    col1, col2, col3 = st.columns(3)
+#     # Create columns for content
+#     col1, col2, col3 = st.columns(3)
 
-    with col1:
-        st.subheader("Case 1")
-        st.write("This is the content for Case 2.")
-        st.markdown("You can add more content here.")
+#     with col1:
+#         st.subheader("Case 1")
+#         st.write("This is the content for Case 2.")
+#         st.markdown("You can add more content here.")
 
-    with col2:
-        st.subheader("Case 2")
-        st.write("You can add content here.")
-        st.markdown("Additional information goes here.")
+#     with col2:
+#         st.subheader("Case 2")
+#         st.write("You can add content here.")
+#         st.markdown("Additional information goes here.")
 
-    with col3:
-        st.subheader("Case 3")
-        st.write("You can add content here.")
-        st.markdown("More information goes here.")
+#     with col3:
+#         st.subheader("Case 3")
+#         st.write("You can add content here.")
+#         st.markdown("More information goes here.")
 
-if tabs3:
-    st.write("Sample Cases")
+# if tabs3:
+#     st.write("Sample Cases")
 
-    # Create columns for content
-    col1, col2, col3 = st.columns(3)
+#     # Create columns for content
+#     col1, col2, col3 = st.columns(3)
 
-    with col1:
-        st.subheader("Case 1")
-        st.write("This is the content for Case 3.")
-        st.markdown("You can add more content here.")
+#     with col1:
+#         st.subheader("Case 1")
+#         st.write("This is the content for Case 3.")
+#         st.markdown("You can add more content here.")
 
-    with col2:
-        st.subheader("Case 2")
-        st.write("You can add content here.")
-        st.markdown("Additional information goes here.")
+    # with col2:
+    #     st.subheader("Case 2")
+    #     st.write("You can add content here.")
+    #     st.markdown("Additional information goes here.")
 
-    with col3:
-        st.subheader("Case 3")
-        st.write("You can add content here.")
-        st.markdown("More information goes here.")
+    # with col3:
+    #     st.subheader("Case 3")
+    #     st.write("You can add content here.")
+    #     st.markdown("More information goes here.")
