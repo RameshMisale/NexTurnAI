4f88d2410be52a12d66336f96ce7d0df30533fcb
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-01 14:10:51 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -11,33 +11,63 @@ st.set_page_config(
 st.image("Logo.png", width=400, use_column_width=False)  # Adjust the width value
 st.title("Welcome to NexTurn AI Project")
 
-# Create tabs for cases
-tabs = st.tabs(["Images", "Videos", "Audios"])
+# Create a radio button for selecting tabs
+selected_tab = st.radio("Select a tab:", ["Images", "Videos", "Audios"])
 
-# Iterate over the tabs
-for i in range(len(tabs)):
+# Create columns for content
+col1, col2, col3 = st.columns(3)
 
-    # Get the tab name
-    tab_name = tabs[i]
+# Add content based on the selected tab
+if selected_tab == "Images":
+    st.subheader("Images Cases")
 
-    # Create a tab item
-    with st.tab_item(tab_name + " Cases"):
+    with col1:
+        st.subheader("Case 1")
+        st.write("This is the content for Images Case 1.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-        # Create columns for content
-        col1, col2, col3 = st.columns(3)
+    with col2:
+        st.subheader("Case 2")
+        st.write("You can add content here for Images Case 2.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-        # Add content to the columns
-        with col1:
-            st.subheader("Case 1")
-            st.write("This is the content for {} Case 1.".format(tab_name))
-            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col3:
+        st.subheader("Case 3")
+        st.write("You can add content here for Images Case 3.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-        with col2:
-            st.subheader("Case 2")
-            st.write("You can add content here for {} Case 2.".format(tab_name))
-            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+elif selected_tab == "Videos":
+    st.subheader("Videos Cases")
 
-        with col3:
-            st.subheader("Case 3")
-            st.write("You can add content here for {} Case 3.".format(tab_name))
-            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col1:
+        st.subheader("Case 1")
+        st.write("This is the content for Videos Case 1.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+
+    with col2:
+        st.subheader("Case 2")
+        st.write("You can add content here for Videos Case 2.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+
+    with col3:
+        st.subheader("Case 3")
+        st.write("You can add content here for Videos Case 3.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+
+elif selected_tab == "Audios":
+    st.subheader("Audios Cases")
+
+    with col1:
+        st.subheader("Case 1")
+        st.write("This is the content for Audios Case 1.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+
+    with col2:
+        st.subheader("Case 2")
+        st.write("You can add content here for Audios Case 2.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+
+    with col3:
+        st.subheader("Case 3")
+        st.write("You can add content here for Audios Case 3.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
