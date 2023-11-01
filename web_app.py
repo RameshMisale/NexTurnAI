b7930da3ce4110a48d14449503866875982c7fa4
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-01 13:52:47 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -12,67 +12,64 @@ st.image("Logo.png", width=400, use_column_width=False)  # Adjust the width valu
 st.title("Welcome to NexTurn AI Project")
 
 # Create tabs for cases
-tabs1,tabs2,tabs3 = st.tabs(["Images", "Videos", "Audios"])
+tabs = st.tabs(["Images", "Videos", "Audios"])
 
-if tabs1 == "Images":
-    st.write("Images Cases")
+if tabs == "Images":
+    with st.tab_item("Images Cases"):
+        # Create columns for content
+        col1, col2, col3 = st.columns(3)
 
-    # Create columns for content
-    col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader("Case 1")
+            st.write("This is the content for Images Case 1.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    with col1:
-        st.subheader("Case 1")
-        st.write("This is the content for Images Case 1.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+        with col2:
+            st.subheader("Case 2")
+            st.write("You can add content here for Images Case 2.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    with col2:
-        st.subheader("Case 2")
-        st.write("You can add content here for Images Case 2.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+        with col3:
+            st.subheader("Case 3")
+            st.write("You can add content here for Images Case 3.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    with col3:
-        st.subheader("Case 3")
-        st.write("You can add content here for Images Case 3.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+if tabs == "Videos":
+    with st.tab_item("Videos Cases"):
+        # Create columns for content
+        col1, col2, col3 = st.columns(3)
 
-if tabs2 == "Videos":
-    st.write("Videos Cases")
+        with col1:
+            st.subheader("Case 1")
+            st.write("This is the content for Videos Case 1.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    # Create columns for content
-    col1, col2, col3 = st.columns(3)
+        with col2:
+            st.subheader("Case 2")
+            st.write("You can add content here for Videos Case 2.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    with col1:
-        st.subheader("Case 1")
-        st.write("This is the content for Videos Case 1.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+        with col3:
+            st.subheader("Case 3")
+            st.write("You can add content here for Videos Case 3.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    with col2:
-        st.subheader("Case 2")
-        st.write("You can add content here for Videos Case 2.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+if tabs == "Audios":
+    with st.tab_item("Audios Cases"):
+        # Create columns for content
+        col1, col2, col3 = st.columns(3)
 
-    with col3:
-        st.subheader("Case 3")
-        st.write("You can add content here for Videos Case 3.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+        with col1:
+            st.subheader("Case 1")
+            st.write("This is the content for Audios Case 1.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-if tabs3 == "Audios":
-    st.write("Audios Cases")
+        with col2:
+            st.subheader("Case 2")
+            st.write("You can add content here for Audios Case 2.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 
-    # Create columns for content
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.subheader("Case 1")
-        st.write("This is the content for Audios Case 1.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
-
-    with col2:
-        st.subheader("Case 2")
-        st.write("You can add content here for Audios Case 2.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
-
-    with col3:
-        st.subheader("Case 3")
-        st.write("You can add content here for Audios Case 3.")
-        st.markdown("[Open in new browser](https://www.example.com)", unsafe_allow_html=True)
+        with col3:
+            st.subheader("Case 3")
+            st.write("You can add content here for Audios Case 3.")
+            st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
