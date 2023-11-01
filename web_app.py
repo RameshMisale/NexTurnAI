63015025e9e548ae877f08e0e27fe6394877dcf3
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-01 16:27:03 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -20,45 +20,64 @@ with tabs1:
     col1, col2, col3 = st.columns(3)
  
     with col1:
-        st.subheader("Details for Case 1")
-        st.write("This is the content for Case 1.")
+        st.subheader("Case 1")
+        st.write("Add the content for Case 1.")
         st.markdown("You can add more content here.")
 
     with col2:
-        st.subheader("Details for Case 2")
-        st.write("This is the content for Case 2.")
+        st.subheader("Case 2")
+        st.write("Add the content for Case 2.")
         st.markdown("You can add more content here.")
 
     with col3:
-        st.subheader("Details for Case 3")
-        st.write("This is the content for Case 3.")
+        st.subheader("Case 3")
+        st.write("Add the content for Case 3.")
         st.markdown("You can add more content here.")
  
     # with col2:
     #     st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
 
 with tabs2:
-    st.write("Content for Case 2")
+    st.write("Content for Video cases")
  
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
  
     with col1:
-        st.subheader("Details for Case 2")
-        st.write("This is the content for Case 2.")
+        st.subheader("Case 1")
+        st.write("Add content for Case 1.")
         st.markdown("You can add more content here.")
- 
+
     with col2:
-        st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
+        st.subheader("Case 2")
+        st.write("Add content for Case 2.")
+        st.markdown("You can add more content here.")
+
+    with col3:
+        st.subheader("Case 3")
+        st.write("Add content for Case 3.")
+        st.markdown("You can add more content here.")
+    # with col2:
+    #     st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
 
 with tabs3:
     st.write("Content for Case 3")
  
-    col1, col2 = st.columns(2)
+    col1, col2,col3 = st.columns(3)
  
     with col1:
-        st.subheader("Details for Case 3")
-        st.write("This is the content for Case 3.")
+        st.subheader("Case 1")
+        st.write("Add content for Case 1.")
+        st.markdown("You can add more content here.")
+
+    with col2:
+        st.subheader("Case 2")
+        st.write("Add content for Case 2.")
+        st.markdown("You can add more content here.")
+
+    with col3:
+        st.subheader("Case 3")
+        st.write("Add content for Case 3.")
         st.markdown("You can add more content here.")
  
-    with col2:
-        st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
+    # with col2:
+    #     st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
