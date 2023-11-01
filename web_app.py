ebfb15cf11a00fd2aff52e53c15c920b6cbd81cd
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-01 14:47:05 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -18,49 +18,57 @@ selected_tab = st.radio("Select a case type:", tabs)
 # Define the content for each case in a dictionary with different links
 cases = {
     "Images": {
-        "Case 1": "This is the content for Images Case 1.",
-        "Case 2": "You can add content here for Images Case 2.",
-        "Case 3": "You can add content here for Images Case 3."
+        "Case 1": {
+            "content": "This is the content for Images Case 1.",
+            "link": "https://www.example.com/images/case1"
+        },
+        "Case 2": {
+            "content": "You can add content here for Images Case 2.",
+            "link": "https://www.example.com/images/case2"
+        },
+        "Case 3": {
+            "content": "You can add content here for Images Case 3.",
+            "link": "https://www.example.com/images/case3"
+        }
     },
     "Videos": {
-        "Case 1": "This is the content for Videos Case 1.",
-        "Case 2": "You can add content here for Videos Case 2.",
-        "Case 3": "You can add content here for Videos Case 3."
+        "Case 1": {
+            "content": "This is the content for Videos Case 1.",
+            "link": "https://www.example.com/videos/case1"
+        },
+        "Case 2": {
+            "content": "You can add content here for Videos Case 2.",
+            "link": "https://www.example.com/videos/case2"
+        },
+        "Case 3": {
+            "content": "You can add content here for Videos Case 3.",
+            "link": "https://www.example.com/videos/case3"
+        }
     },
     "Audios": {
-        "Case 1": "This is the content for Audios Case 1.",
-        "Case 2": "You can add content here for Audios Case 2.",
-        "Case 3": "You can add content here for Audios Case 3."
+        "Case 1": {
+            "content": "This is the content for Audios Case 1.",
+            "link": "https://www.example.com/audios/case1"
+        },
+        "Case 2": {
+            "content": "You can add content here for Audios Case 2.",
+            "link": "https://www.example.com/audios/case2"
+        },
+        "Case 3": {
+            "content": "You can add content here for Audios Case 3.",
+            "link": "https://www.example.com/audios/case3"
+        }
     }
 }
 
-# Define links for each case type
-links = {
-    "Images": {
-        "Case 1": "https://www.example.com/images/case1",
-        "Case 2": "https://www.example.com/images/case2",
-        "Case 3": "https://www.example.com/images/case3"
-    },
-    "Videos": {
-        "Case 1": "https://www.example.com/videos/case1",
-        "Case 2": "https://www.example.com/videos/case2",
-        "Case 3": "https://www.example.com/videos/case3"
-    },
-    "Audios": {
-        "Case 1": "https://www.example.com/audios/case1",
-        "Case 2": "https://www.example.com/audios/case2",
-        "Case 3": "https://www.example.com/audios/case3"
-    }
-}
-
-# Display the selected tab's cases with links in side-by-side columns
+# Display the selected tab's cases with links, subheaders, and content in side-by-side columns
 if selected_tab in cases:
     tab_content = cases[selected_tab]
-    tab_links = links[selected_tab]
     col1, col2, col3 = st.columns(3)
-    for case, content in tab_content.items():
-        st.subheader(case)
-        col = col1 if case == "Case 1" else (col2 if case == "Case 2" else col3)
-        with col:
-            st.write(content)
-            st.markdown(f"[Open {case}]({tab_links[case]})", unsafe_allow_html=True)
+    for case, case_data in tab_content.items():
+        with col1:
+            st.subheader(case)
+        with col2:
+            st.write(case_data["content"])
+        with col3:
+            st.markdown(f"[Open {case}]({case_data['link']})", unsafe_allow_html=True)
