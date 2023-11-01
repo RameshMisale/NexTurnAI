6db408f5ec9e628f463b81b34f52b67fb857887b
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-01 16:12:40 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -183,93 +183,93 @@ st.markdown("---")
 
 st.write("Thank you for visiting...!!")
 
-[Monday 15:02] Ramesh Misale
-import streamlit as st
+# [Monday 15:02] Ramesh Misale
+# import streamlit as st
  
-# Set page title and company logo
+# # Set page title and company logo
 
-st.set_page_config(
+# st.set_page_config(
 
-    page_title="NexTurn AI Project",
+#     page_title="NexTurn AI Project",
 
-    page_icon=":rocket:",
+#     page_icon=":rocket:",
 
-    layout="wide"
+#     layout="wide"
 
-)
+# )
  
-# Logo
+# # Logo
 
-st.image("Logo.jpg")
+# st.image("Logo.jpg")
 
-st.title("Welcome to NexTurn AI Project")
+# st.title("Welcome to NexTurn AI Project")
  
-# Create tabs for cases
+# # Create tabs for cases
 
-tabs = st.tabs(["Case 1", "Case 2", "Case 3"])
+# tabs = st.tabs(["Case 1", "Case 2", "Case 3"])
  
-if tabs == "Case 1":
+# if tabs == "Case 1":
 
-    st.write("Content for Case 1")
+#     st.write("Content for Case 1")
  
-    # Create columns for content
+#     # Create columns for content
 
-    col1, col2 = st.columns(2)
+#     col1, col2 = st.columns(2)
  
-    with col1:
+#     with col1:
 
-        st.subheader("Details for Case 1")
+#         st.subheader("Details for Case 1")
 
-        st.write("This is the content for Case 1.")
+#         st.write("This is the content for Case 1.")
 
-        st.markdown("You can add more content here.")
+#         st.markdown("You can add more content here.")
  
-    with col2:
+#     with col2:
 
-        # Open a new tab with HTML link
+#         # Open a new tab with HTML link
 
-        st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
+#         st.markdown("<a href='https://your-case1-link.com' target='_blank'>Open Case 1</a>", unsafe_allow_html=True)
  
-elif tabs == "Case 2":
+# elif tabs == "Case 2":
 
-    st.write("Content for Case 2")
+#     st.write("Content for Case 2")
  
-    # Create columns for content
+#     # Create columns for content
 
-    col1, col2 = st.columns(2)
+#     col1, col2 = st.columns(2)
  
-    with col1:
+#     with col1:
 
-        st.subheader("Details for Case 2")
+#         st.subheader("Details for Case 2")
 
-        st.write("This is the content for Case 2.")
+#         st.write("This is the content for Case 2.")
 
-        st.markdown("You can add more content here.")
+#         st.markdown("You can add more content here.")
  
-    with col2:
+#     with col2:
 
-        # Open a new tab with HTML link
+#         # Open a new tab with HTML link
 
-        st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
+#         st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
  
-elif tabs == "Case 3":
+# elif tabs == "Case 3":
 
-    st.write("Content for Case 3")
+#     st.write("Content for Case 3")
  
-    # Create columns for content
+#     # Create columns for content
 
-    col1, col2 = st.columns(2)
+#     col1, col2 = st.columns(2)
  
-    with col1:
+#     with col1:
 
-        st.subheader("Details for Case 3")
+#         st.subheader("Details for Case 3")
 
-        st.write("This is the content for Case 3.")
+#         st.write("This is the content for Case 3.")
 
-        st.markdown("You can add more content here.")
+#         st.markdown("You can add more content here.")
  
-    with col2:
+#     with col2:
 
-        # Open a new tab with HTML link
+#         # Open a new tab with HTML link
 
-        st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
+#         st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
