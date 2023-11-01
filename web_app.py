68630d5ff1ec940cf368d9fa85259f66eae2916e
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-01 15:16:37 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -61,14 +61,12 @@ cases = {
     }
 }
 
-# Display the selected tab's cases with links, subheaders, and content side by side
+# Display the selected tab's cases with links, subheaders, and content in side-by-side columns
 if selected_tab in cases:
     tab_content = cases[selected_tab]
     col1, col2, col3 = st.columns(3)
     for case, case_data in tab_content.items():
         with col1:
             st.subheader(case)
-        with col2:
             st.write(case_data["content"])
-        with col3:
             st.markdown(f"[Open {case}]({case_data['link']})", unsafe_allow_html=True)
