ce061ed9c2d391e64cc87dd7e7d40833023e62ee
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-02 14:20:09 +0530

    Update house_price_prediction.py

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -1,36 +1,22 @@
 import streamlit as st
 import joblib
 
-# Load the pre-trained machine learning model
-model = joblib.load('Regression_model.pkl')  # Replace with the actual path to your model file
+st.title("House Price Prediction")
 
-# Set page title and company logo
-st.set_page_config(
-    page_title="House Price Prediction",
-    page_icon=":house:",
-    layout="wide"
-)
+# Create input fields for user input
+price = st.number_input("Price")
+sqft = st.number_input("SqFt")
+bedrooms = st.number_input("Bedrooms")
+bathrooms = st.number_input("Bathrooms")
+offers = st.number_input("Offers")
+brick = st.checkbox("Brick")
+neighborhood = st.selectbox("Neighborhood", ["Neighborhood A", "Neighborhood B", "Neighborhood C"])
 
-# Check if the query parameter "page" is set to "house_price_prediction"
-if "page" in st.experimental_get_query_params():
-    if st.experimental_get_query_params()["page"] == ["house_price_prediction"]:
-        st.title("House Price Prediction")
-
-        # Create input fields for user input
-        sqft = st.number_input("SqFt", min_value=0)
-        bedrooms = st.number_input("Bedrooms", min_value=0)
-        bathrooms = st.number_input("Bathrooms", min_value=0)
-        offers = st.number_input("Offers", min_value=0)
-        brick = st.checkbox("Brick")
-        neighborhood = st.selectbox("Neighborhood", ["Neighborhood A", "Neighborhood B", "Neighborhood C"])
-
-        if st.button("Predict Price"):
-            # Prepare the input data for prediction
-            input_data = [sqft, bedrooms, bathrooms, offers, int(brick), neighborhood]
-
-            # Make the prediction using the model
-            predicted_price = model.predict([input_data])[0]
-
-            # Display the predicted price
-            st.subheader("Predicted Price:")
-            st.write(f"${predicted_price:,.2f}")
+if st.button("Predict Price"):
+    # Load your pre-trained regression model
+    model = joblib.load('Regression_model.pkl')
+    # Prepare input data for prediction
+    input_data = [price, sqft, bedrooms, bathrooms, offers, int(brick), neighborhood]
+    # Make the prediction
+    predicted_price = model.predict([input_data])[0]
+    st.write(f"Predicted Price: ${predicted_price:,.2f}")
