f6c28aea1b54e0b498da1ec426ca97f300b9e3dd
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-02 11:02:34 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,61 +1,58 @@
 import streamlit as st
+
 # Set page title and company logo
 st.set_page_config(
     page_title="NexTurn AI Project",
     page_icon=":rocket:",
     layout="wide"
 )
+
 # Logo and title with an increased size
-st.image("Logo.png", width=400, use_column_width=False)  # Increase the width value to adjust the logo size
+st.image("Logo.png", width=400, use_column_width=False)
 st.title("Welcome to NexTurn AI Project")
+
 # Create tabs for cases
-tabs1, tabs2, tabs3 = st.tabs(["Images", "Videos", "Audios"])
+tabs1, tabs2, tabs3 = st.tabs(["Case 1", "Case 2", "Case 3"])
+
 with tabs1:
-    st.write("Content for image Cases")
-    col1, col2, col3 = st.columns(3) 
-    with col1:
-        st.subheader("Case 1")
-        st.write("Add the content for Case 1.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    with col2:
-        st.subheader("Case 2")
-        st.write("Add the content for Case 2.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    with col3:
-        st.subheader("Case 3")
-        st.write("Add the content for Case 3.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-with tabs2:
-    st.write("Content for Video Cases") 
-    col1, col2, col3 = st.columns(3) 
-    with col1:
-        st.subheader("Case 1")
-        st.write("Add content for Case 1.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True) 
-    with col2:
-        st.subheader("Case 2")
-        st.write("Add content for Case 2.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    with col3:
-        st.subheader("Case 3")
-        st.write("Add content for Case 3.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    st.write("Content for Case 1")
  
-with tabs3:
-    st.write("Content for Audio Cases") 
-    col1, col2,col3 = st.columns(3) 
+    col1, col2 = st.columns(2)
+ 
     with col1:
-        st.subheader("Case 1")
-        st.write("Add content for Case 1.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+        st.subheader("Details for Case 1")
+        st.write("This is the content for Case 1.")
+        st.markdown("You can add more content here.")
+ 
     with col2:
-        st.subheader("Case 2")
-        st.write("Add content for Case 2.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    with col3:
-        st.subheader("Case 3")
-        st.write("Add content for Case 3.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
- # Footer
-st.markdown("---")
-st.write("Thank you for visiting...!!")        
+        # Open a new tab with HTML link and ML model prediction
+        if st.button("Open Case 1"):
+            # Replace the URL below with your ML model endpoint
+            ml_model_url = "https://your-ml-model-endpoint.com"
+            st.markdown(f"<a href='{ml_model_url}' target='_blank'>Open Case 1 Prediction</a>", unsafe_allow_html=True)
+
+with tabs2:
+    st.write("Content for Case 2")
+ 
+    col1, col2 = st.columns(2)
+ 
+    with col1:
+        st.subheader("Details for Case 2")
+        st.write("This is the content for Case 2.")
+        st.markdown("You can add more content here.")
+ 
+    with col2:
+        st.markdown("<a href='https://your-case2-link.com' target='_blank'>Open Case 2</a>", unsafe_allow_html=True)
+
+with tabs3:
+    st.write("Content for Case 3")
+ 
+    col1, col2 = st.columns(2)
+ 
+    with col1:
+        st.subheader("Details for Case 3")
+        st.write("This is the content for Case 3.")
+        st.markdown("You can add more content here.")
+ 
+    with col2:
+        st.markdown("<a href='https://your-case3-link.com' target='_blank'>Open Case 3</a>", unsafe_allow_html=True)
