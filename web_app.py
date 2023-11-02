28a31956363d94c49a3c29cf7282d32275dd1bbc
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-02 12:00:26 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+# Load the pre-trained machine learning model
+model = joblib.load('Regression_model.pkl')  # Replace with the actual path to your model file
 
 # Set page title and company logo
 st.set_page_config(
@@ -19,10 +25,9 @@ with tabs1:
 
     with col1:
         st.subheader("Case 1")
-        st.write("Click the button to open the House Price Prediction page")
+        st.write("Click the button to open the House Price Prediction GUI")
         if st.button("Open House Price Prediction"):
-            # Open the HTML page from your GitHub repository in a new tab
-            st.markdown("<a href='https://raw.githubusercontent.com/RameshMisale/NexTurnAI/main/house_price_prediction.html' target='_blank'>Open House Price Prediction</a>", unsafe_allow_html=True)
+            st.markdown("[Open House Price Prediction](https://your-app-url.com/house_price_prediction)", unsafe_allow_html=True)
 
     with col2:
         st.subheader("Case 2")
@@ -33,5 +38,28 @@ with tabs1:
         st.subheader("Case 3")
         st.write("Add the content for Case 3.")
         st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+
+# Create a separate Streamlit page for House Price Prediction GUI
+if st.url == "https://your-app-url.com/house_price_prediction":
+    st.title("House Price Prediction")
+
+    # Create input fields for user input
+    sqft = st.number_input("SqFt", min_value=0)
+    bedrooms = st.number_input("Bedrooms", min_value=0)
+    bathrooms = st.number_input("Bathrooms", min_value=0)
+    offers = st.number_input("Offers", min_value=0)
+    brick = st.checkbox("Brick")
+    neighborhood = st.selectbox("Neighborhood", ["Neighborhood A", "Neighborhood B", "Neighborhood C"])
+
+    if st.button("Predict Price"):
+        # Prepare the input data for prediction
+        input_data = [sqft, bedrooms, bathrooms, offers, int(brick), neighborhood]
+
+        # Make the prediction using the model
+        predicted_price = model.predict([input_data])[0]
+
+        # Display the predicted price
+        st.subheader("Predicted Price:")
+        st.write(f"${predicted_price:,.2f}")
 
 # Rest of your code...
