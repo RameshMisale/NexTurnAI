e701f42541e6814a7ad1ac2a055f6747fbfe5206
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-02 12:47:58 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,53 +1,61 @@
 import streamlit as st
-
 # Set page title and company logo
 st.set_page_config(
     page_title="NexTurn AI Project",
     page_icon=":rocket:",
     layout="wide"
 )
-
 # Logo and title with an increased size
-st.image("Logo.png", width=400, use_column_width=False)
+st.image("Logo.png", width=400, use_column_width=False)  # Increase the width value to adjust the logo size
 st.title("Welcome to NexTurn AI Project")
-
 # Create tabs for cases
-tabs1, tabs2, tabs3 = st.columns(3)
-
+tabs1, tabs2, tabs3 = st.tabs(["Images", "Videos", "Audios"])
 with tabs1:
-    st.subheader("Case 1")
-    st.write("Click the button to open the House Price Prediction GUI")
-    if st.button("Open House Price Prediction"):
-        # Redirect to the House Price Prediction page using query parameters
-        st.experimental_set_query_params(page="house_price_prediction")
-
+    st.write("Content for image Cases")
+    col1, col2, col3 = st.columns(3) 
+    with col1:
+        st.subheader("Case 1")
+        st.write("Add the content for Case 1.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col2:
+        st.subheader("Case 2")
+        st.write("Add the content for Case 2.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col3:
+        st.subheader("Case 3")
+        st.write("Add the content for Case 3.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
 with tabs2:
-    st.subheader("Case 2")
-    st.write("Add the content for Case 2.")
-
+    st.write("Content for Video Cases") 
+    col1, col2, col3 = st.columns(3) 
+    with col1:
+        st.subheader("Case 1")
+        st.write("Add content for Case 1.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True) 
+    with col2:
+        st.subheader("Case 2")
+        st.write("Add content for Case 2.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col3:
+        st.subheader("Case 3")
+        st.write("Add content for Case 3.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+ 
 with tabs3:
-    st.subheader("Case 3")
-    st.write("Add the content for Case 3.")
-
-# Check if the query parameter "page" is set to "house_price_prediction"
-if "page" in st.experimental_get_query_params() and st.experimental_get_query_params()["page"] == "house_price_prediction.py":
-    st.title("House Price Prediction")
-
-    # Create input fields for user input
-    sqft = st.number_input("SqFt", min_value=0)
-    bedrooms = st.number_input("Bedrooms", min_value=0)
-    bathrooms = st.number_input("Bathrooms", min_value=0)
-    offers = st.number_input("Offers", min_value=0)
-    brick = st.checkbox("Brick")
-    neighborhood = st.selectbox("Neighborhood", ["Neighborhood A", "Neighborhood B", "Neighborhood C"])
-
-    if st.button("Predict Price"):
-        # Prepare the input data for prediction
-        input_data = [sqft, bedrooms, bathrooms, offers, int(brick), neighborhood]
-
-        # Make the prediction using the model
-        predicted_price = model.predict([input_data])[0]
-
-        # Display the predicted price
-        st.subheader("Predicted Price:")
-        st.write(f"${predicted_price:,.2f}")
+    st.write("Content for Audio Cases") 
+    col1, col2,col3 = st.columns(3) 
+    with col1:
+        st.subheader("Case 1")
+        st.write("Add content for Case 1.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col2:
+        st.subheader("Case 2")
+        st.write("Add content for Case 2.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    with col3:
+        st.subheader("Case 3")
+        st.write("Add content for Case 3.")
+        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+ # Footer
+st.markdown("---")
+st.write("Thank you for visiting...!!")  
