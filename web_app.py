f461b986a40f17fd6e1b4ffd02428905208ccbdd
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-11-04 00:40:57 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,73 +1,88 @@
 import streamlit as st
+import pickle
+import pandas as pd
 
-# Set page title and company logo
 st.set_page_config(
     page_title="NexTurn AI Project",
     page_icon=":rocket:",
     layout="wide"
 )
 
-# Logo and title with an increased size
 st.image("Logo.png", width=400, use_column_width=False)
 st.title("Welcome to NexTurn AI Project")
 
 # Create tabs for cases
 tabs1, tabs2, tabs3 = st.tabs(["General", "Text", "Images"])
+
+# House Price Prediction Section
 with tabs1:
-    st.write("General Cases")
-    col1, col2, col3 = st.columns(3)
+    st.write("House Price Prediction (Regression)")
+    
+    # Create input fields for user input
+    st.sidebar.title('Input Features')
+    age = st.sidebar.number_input("Age")
+    sqft = st.sidebar.number_input("SqFt")
+    bedrooms = st.sidebar.number_input("Bedrooms")
+    bathrooms = st.sidebar.number_input("Bathrooms")
+    offers = st.sidebar.number_input("Offers")
+    brick = st.sidebar.checkbox("Brick")
 
-    with col1:
-        st.subheader("Case 1")
-        st.write("House Price Prediction(Regression)")
-        #if st.button("Open House Price Prediction"):
-        st.markdown("[Open House Price Prediction](https://appgui-jk6qksscd9ckag6lmcemz9.streamlit.app/)", unsafe_allow_html=True)
+    # Load the pre-trained regression model using pickle
+    with open('Regression_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
 
+    # Create a DataFrame with the input data
+    input_data = pd.DataFrame({
+        'Age': [age],
+        'SqFt': [sqft],
+        'Bedrooms': [bedrooms],
+        'Bathrooms': [bathrooms],
+        'Offers': [offers],
+        'Brick': [int(brick)]
+    })
 
-    with col2:
-        st.subheader("Case 2")
-        st.write("Canser Prediction(Classification)")
-        #st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-        st.markdown("[Open Canser Prediction](https://classificationmodel-ambas3ka2x2zodupzgkb98.streamlit.app/)", unsafe_allow_html=True)
+    if st.sidebar.button("Predict House Price"):
+        # Make the prediction
+        predicted_price = model.predict(input_data)[0]
+        st.write(f"Predicted House Price: ${predicted_price:,.2f}")
 
-    with col3:
-        st.subheader("Case 3")
-        st.write("Add the content for Case 3.")
-        st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-
+# Cancer Prediction Section
 with tabs2:
-    st.write("Content is under construction...")
-    st.write("!!!...COMING SOON..!!!")
-    # col1, col2, col3 = st.columns(3)
-    # with col1:
-    #     st.subheader("Case 1")
-    #     st.write("Add content for Case 1.")
-    #     st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    # with col2:
-    #     st.subheader("Case 2")
-    #     st.write("Add content for Case 2.")
-    #     st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    # with col3:
-    #     st.subheader("Case 3")
-    #     st.write("Add content for Case 3.")
-    #     st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    st.write("Cancer Prediction (Classification)")
+    
+    # Create input fields for user input
+    st.sidebar.title('Input Features')
+    age = st.sidebar.number_input("Age")
+    bmi = st.sidebar.number_input("BMI")
+    glucose = st.sidebar.number_input("Glucose")
+    insulin = st.sidebar.number_input("Insulin")
+    homa = st.sidebar.number_input("HOMA")
+    leptin = st.sidebar.number_input("Leptin")
+    adiponectin = st.sidebar.number_input("Adiponectin")
+    resistin = st.sidebar.number_input("Resistin")
+    mcp1 = st.sidebar.number_input("MCP.1")
 
-with tabs3:
-    st.write("Content is under construction...")
-    st.write("!!!...COMING SOON..!!!")
-    # col1, col2, col3 = st.columns(3)
-    # with col1:
-    #     st.subheader("Case 1")
-    #     st.write("Add content for Case 1.")
-    #     st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    # with col2:
-    #     st.subheader("Case 2")
-    #     st.write("Add content for Case 2.")
-    #     st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
-    # with col3:
-    #     st.subheader("Case 3")
-    #     st.write("Add content for Case 3.")
-    #     st.markdown("[Open in a new browser](https://www.example.com)", unsafe_allow_html=True)
+    # Load the pre-trained classification model using pickle
+    with open('Classification_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+
+    # Create a DataFrame with the input data
+    input_data = pd.DataFrame({
+        'Age': [age],
+        'BMI': [bmi],
+        'Glucose': [glucose],
+        'Insulin': [insulin],
+        'HOMA': [homa],
+        'Leptin': [leptin],
+        'Adiponectin': [adiponectin],
+        'Resistin': [resistin],
+        'MCP.1': [mcp1]
+    })
+
+    if st.sidebar.button("Predict Cancer"):
+        # Make the prediction
+        predicted_class = model.predict(input_data)[0]
+        st.write(f"Predicted Cancer Class: {predicted_class}")
 
 # Footer
 st.markdown("---")
